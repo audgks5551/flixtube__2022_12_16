c2f1142f25380bd3baa6e08e418fb7bacb92c7a5
myunghan <myunghan@myunghanui-iMac.local>
2022-12-19 09:03:11 +0900

    비디오 스트리밍 간단 api 구현 및 port를 환경변수로 설정

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-const port = 3000
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT");
+}
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-})
+const port = process.env.PORT;
+
+app.get("/video", (req, res) => {
+    const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.error("An error occurred");
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4", 
+        });
+
+        fs.createReadStream(path).pipe(res);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
-})
+});
